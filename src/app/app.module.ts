212d5928981ba8a4ad5d976b92d98890bd93e031
natinan123 <43422484+natinan123@users.noreply.github.com>
2019-12-22 00:37:48 +0700

    New

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,36 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AdminComponent } from './admin/admin.component';
+import { SellerComponent } from './seller/seller.component';
+import { BuyerComponent } from './buyer/buyer.component';
+import { MainpageComponent } from './mainpage/mainpage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminComponent,
+    SellerComponent,
+    BuyerComponent,
+    MainpageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
+ //  อาจจะไม่ใช้
+//  "@angular/platform-server": "^8.0.0",
+//  "angular-alert-module": "^2.0.3",
+//  "angular-authentication-service": "^1.1.8",
+//  "angular-font-awesome": "^3.1.2",
+//  "angular-image-slider": "0.0.8",
+//  "font-awesome": "^4.7.0",
+//  "ngx-papaparse": "^3.0.2",
---
+++ src/app/mainpage/mainpage.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { MainpageRoutingModule } from './mainpage-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MainpageRoutingModule
+  ]
+})
+export class MainpageModule { }
